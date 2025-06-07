c47704a166f4fc6d5c1b5b5b2cbd22bb4a0de409
jonas-amilton <jonas96@edu.unisinos.br>
2025-06-07 20:47:55 -0300

    chore: nome da aplicação

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Gerenciador de café da firma'),
 
     /*
     |--------------------------------------------------------------------------
