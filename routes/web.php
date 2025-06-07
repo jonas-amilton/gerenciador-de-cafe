d06612d0e8e49577330b6dbfe846328255d5bc75
jonas-amilton <jonas96@edu.unisinos.br>
2025-06-07 20:54:50 -0300

    refactor: removido rota da página de boas vindas, e alterado para dashboard ser a página inicial

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,39 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CoffeePreparationController;
+use App\Http\Controllers\CoffeeConsumptionController;
+use App\Http\Controllers\CoffeePurchaseController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::view('/', 'welcome');
-
-Route::prefix('coffee')->group(function () {
+Route::middleware('auth')->prefix('coffee')->group(function () {
     Route::view('/purchases', 'coffee.purchases.index')->name('coffee.purchases.index');
     Route::view('/consumptions', 'coffee.consumptions.index')->name('coffee.consumptions.index');
     Route::view('/preparations', 'coffee.preparations.index')->name('coffee.preparations.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->prefix('coffee/preparations')->name('coffee.preparations.')->group(function () {
+    Route::get('/', [CoffeePreparationController::class, 'index'])->name('index');
+    Route::get('/create', [CoffeePreparationController::class, 'create'])->name('create');
+    Route::post('/', [CoffeePreparationController::class, 'store'])->name('store');
+});
+
+Route::middleware('auth')->prefix('coffee/consumptions')->name('coffee.consumptions.')->group(function () {
+    Route::get('/', [CoffeeConsumptionController::class, 'index'])->name('index');
+    Route::get('/create', [CoffeeConsumptionController::class, 'create'])->name('create');
+    Route::post('/', [CoffeeConsumptionController::class, 'store'])->name('store');
+});
+
+Route::middleware('auth')->prefix('coffee/purchases')->name('coffee.purchases.')->group(function () {
+    Route::get('/', [CoffeePurchaseController::class, 'index'])->name('index');
+    Route::get('/create', [CoffeePurchaseController::class, 'create'])->name('create');
+    Route::post('/', [CoffeePurchaseController::class, 'store'])->name('store');
+});
+
+Route::get('/', [HomeController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -25,4 +41,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
