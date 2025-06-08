5c71129484decdc2c601c3858e62bb665c5473e6
jonas-amilton <jonas96@edu.unisinos.br>
2025-06-08 20:34:48 -0300

    fix: corrigido form para ser genérico

--- resources/views/coffee/purchases/partials/form.blade.php
+++ resources/views/coffee/purchases/partials/form.blade.php
@@ -3,7 +3,7 @@
     <select name="user_id" id="user_id" class="form-select" required>
         <option value="">Selecionar comprador</option>
         @foreach ($users as $user)
-            <option value="{{ $user->id }}" {{ old('user_id', $purchase->user_id) == $user->id ? 'selected' : '' }}>
+            <option value="{{ $user->id }}" {{ old('user_id', $purchase->user_id ?? null) == $user->id ? 'selected' : '' }}>
                 {{ $user->name }}
             </option>
         @endforeach
@@ -13,14 +13,14 @@
 <div class="mb-3">
     <label for="purchased_at" class="form-label">Data da compra</label>
     <input type="date"
-        value="{{ old('purchased_at', \Carbon\Carbon::parse($purchase->purchased_at)->format('Y-m-d')) }}"
+        value="{{ old('purchased_at', isset($purchase->purchased_at) ? \Carbon\Carbon::parse($purchase->purchased_at)->format('Y-m-d') : '') }}"
         name="purchased_at" id="purchased_at" class="form-control" required>
 </div>
 
 <div class="mb-3">
     <label for="quantity" class="form-label">Quantidade (unidade)</label>
     <input type="number" name="quantity" id="quantity" class="form-control"
-        value="{{ old('quantity', $purchase->quantity) }}" required min="1">
+        value="{{ old('quantity', $purchase->quantity ?? null) }}" required min="1">
 </div>
 
 <button type="submit" class="btn btn-success">Enviar</button>
