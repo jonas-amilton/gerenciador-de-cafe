8e47c8dc42fd7fd3b6c623286e9d9456f28ff20f
jonas-amilton <jonas96@edu.unisinos.br>
2025-06-07 21:37:08 -0300

    refactor: organizado rotas com resource

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,18 @@
 <?php
-
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CoffeePreparationController;
-use App\Http\Controllers\CoffeeConsumptionController;
-use App\Http\Controllers\CoffeePurchaseController;
 
-Route::middleware('auth')->prefix('coffee')->group(function () {
-    Route::view('/purchases', 'coffee.purchases.index')->name('coffee.purchases.index');
-    Route::view('/consumptions', 'coffee.consumptions.index')->name('coffee.consumptions.index');
-    Route::view('/preparations', 'coffee.preparations.index')->name('coffee.preparations.index');
+Route::middleware('auth')->prefix('coffee')->name('coffee.')->group(function () {
+    Route::resource('consumptions', App\Http\Controllers\CoffeeConsumptionController::class)->names('consumptions');
+    Route::resource('preparations', App\Http\Controllers\CoffeePreparationController::class)->names('preparations');
+    Route::resource('purchases', App\Http\Controllers\CoffeePurchaseController::class)->names('purchases');
 });
 
-Route::middleware('auth')->prefix('coffee/preparations')->name('coffee.preparations.')->group(function () {
-    Route::get('/', [CoffeePreparationController::class, 'index'])->name('index');
-    Route::get('/create', [CoffeePreparationController::class, 'create'])->name('create');
-    Route::post('/', [CoffeePreparationController::class, 'store'])->name('store');
-});
-
-Route::middleware('auth')->prefix('coffee/consumptions')->name('coffee.consumptions.')->group(function () {
-    Route::get('/', [CoffeeConsumptionController::class, 'index'])->name('index');
-    Route::get('/create', [CoffeeConsumptionController::class, 'create'])->name('create');
-    Route::post('/', [CoffeeConsumptionController::class, 'store'])->name('store');
-});
-
-Route::middleware('auth')->prefix('coffee/purchases')->name('coffee.purchases.')->group(function () {
-    Route::get('/', [CoffeePurchaseController::class, 'index'])->name('index');
-    Route::get('/create', [CoffeePurchaseController::class, 'create'])->name('create');
-    Route::post('/', [CoffeePurchaseController::class, 'store'])->name('store');
-});
-
-Route::get('/', [HomeController::class, 'index'])
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->prefix('profile')->name('profile.')->group(function () {
+    Route::resource('/', App\Http\Controllers\ProfileController::class)->only(['edit', 'update', 'destroy']);
 });
 
 require __DIR__ . '/auth.php';
